52e6a5ad2c1cc01a235073402276814c8e54cc99
Djole33 <pandajedetrsku@gmail.com>
2024-08-15 23:37:28 +0200

    added basic functionality

--- main/views.py
+++ main/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Item
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World!")
+    item_objects = Item.objects.all()
+    return render(request, template_name='food/index.html', context={'item_objects': item_objects})
 
 def item(request):
     return HttpResponse("<h1>Hello Items!</h1>")
+
+def detail(request, pk):
+    item = Item.objects.get(id=pk)
+    return render(request, 'food/detail.html', context={'item': item})
