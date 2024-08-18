601b3c59faf31fb2a6ecafb211de701a2216b234
Djole33 <pandajedetrsku@gmail.com>
2024-08-18 17:26:16 +0200

    'auth'

---
+++ users/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import RegisterFrom
+
+# Create your views here.
+
+def register(request):
+    form = RegisterFrom()   
+    if request.method == "POST":
+        form = RegisterFrom(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Welcome, {username}!')
+            return redirect('/')
+    return render(request, 'users/register.html', {'form': form})
